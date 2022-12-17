43b718da9d76e1cd52d7726b8865f56aecef0bd5
Ryu236 <ryu.adv.2360@gmail.com>
2022-12-17 23:43:09 +0900

    :recycle: refactor(module): add module file

--- src/main/kotlin/com/ryu236/Application.kt
+++ src/main/kotlin/com/ryu236/Application.kt
@@ -1,12 +1,12 @@
 package com.ryu236
 
+import com.ryu236.plugins.configureRouting
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.ryu236.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
 
